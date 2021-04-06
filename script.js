c31a6dc58f84cb276563df22513eb45b07f031f3
Paulo Thor <paulothor12345@gmail.com>
2021-04-06 17:02:22 -0300

    ajustando conflitos

--- script.js
+++ script.js
@@ -19,41 +19,6 @@ const main = document.getElementById("gameBoard")
 let currentPlayer = "Player1"
 
 
-function checkDiagonalTopRight(col,line) {
-    const columnExists = 
-    console.log(col,line)
-
-    let a = map[col+1][line+1] | 0
-    console.log(a)
-    // map[col][line], map[col+1][line+1], map[col+2][line+2], map[col+3][line+3])
-}
-
-function checkVictoryDiagonal(col,line) {
-    // function check(a = -1,b = -1,c = -1,d = -1) {
-    //     if (a === -1 || b === -1 || c === -1 || d === -1) {return false}
-    //     let sequenceDiv = [a,b,c,d] 
-    //     for (let i = 1; i < sequenceDiv.length; i++) {
-    //         if (sequenceDiv[i] !== sequenceDiv[i-1]) {
-    //             return true
-    //         }
-    //     }
-    // }
-    const current = map[col][line]
-    if (current === map[col+1][line+1] && current === map[col+2][line+2] && current === map[col+3][line+3]) {
-        return true
-    }
-    if (current === map[col-1][line-1] && current === map[col-2][line-2] && current === map[col-3][line-3]) {
-        return true
-    }
-    if (current === map[col+1][line-1] && current === map[col+2][line-2] && current === map[col+3][line-3]) {
-        return true
-    }
-    if (current === map[col-1][line+1] && current === map[col-2][line+2] && current === map[col-3][line+3]) {
-        return true
-    }
-    return false
-}
-
 //  checar vitoria
 function checkVictory(col,line){
 
@@ -193,8 +158,8 @@ const moveCircle = (e) => {
         mapPosition.classList.add('player2')
         map[columnOnMap][lineOnMap] = 2
     }
-    checkVictoryDiagonal(columnOnMap,lineOnMap)
-    // checkVictory(columnOnMap,lineOnMap)
+    // checkVictoryDiagonal(columnOnMap,lineOnMap)
+    checkVictory(columnOnMap,lineOnMap)
     checkDraw()
     changePlayer()
 
