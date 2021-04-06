508fcaef17fe9a47df224a5e613c9555841fbf77
Paulo Thor <paulothor12345@gmail.com>
2021-04-06 13:04:34 -0300

    tentando começar

--- script.js
+++ script.js
@@ -18,6 +18,42 @@ const main = document.getElementById("gameBoard")
 
 let currentPlayer = "Player1"
 
+
+function checkDiagonalTopRight(col,line) {
+    const columnExists = 
+    console.log(col,line)
+
+    let a = map[col+1][line+1] | 0
+    console.log(a)
+    // map[col][line], map[col+1][line+1], map[col+2][line+2], map[col+3][line+3])
+}
+
+function checkVictoryDiagonal(col,line) {
+    // function check(a = -1,b = -1,c = -1,d = -1) {
+    //     if (a === -1 || b === -1 || c === -1 || d === -1) {return false}
+    //     let sequenceDiv = [a,b,c,d] 
+    //     for (let i = 1; i < sequenceDiv.length; i++) {
+    //         if (sequenceDiv[i] !== sequenceDiv[i-1]) {
+    //             return true
+    //         }
+    //     }
+    // }
+    const current = map[col][line]
+    if (current === map[col+1][line+1] && current === map[col+2][line+2] && current === map[col+3][line+3]) {
+        return true
+    }
+    if (current === map[col-1][line-1] && current === map[col-2][line-2] && current === map[col-3][line-3]) {
+        return true
+    }
+    if (current === map[col+1][line-1] && current === map[col+2][line-2] && current === map[col+3][line-3]) {
+        return true
+    }
+    if (current === map[col-1][line+1] && current === map[col-2][line+2] && current === map[col-3][line+3]) {
+        return true
+    }
+    return false
+}
+
 //  checar vitoria
 function checkVictory(col,line){
 
@@ -76,16 +112,17 @@ for(let i = 0; i < map.length; i++) {
     } }
 
 
-
+column2
+column3
 
 const moveCircle = (e) => {
     const currentColumn = e.currentTarget;
-    const columnOnMap = currentColumn.id.substring(6)
-    const columnIsFull = map[columnOnMap].indexOf(0) === -1
+    const columnOnMap = Number(currentColumn.id.substring(6))
+    const columnIsFull = map[columnOnMap].indexOf(0) === -1 
 
     if (columnIsFull) {return} 
 
-    const lineOnMap = map[columnOnMap].indexOf(0)
+    const lineOnMap = map[columnOnMap].indexOf(0) 
 
     const mapPosition = document.getElementById(`${columnOnMap}-${lineOnMap}`)
     
@@ -97,15 +134,15 @@ const moveCircle = (e) => {
         mapPosition.classList.add('player2')
         map[columnOnMap][lineOnMap] = 2
     }
-
-    checkVictory(columnOnMap,lineOnMap)
+    checkVictoryDiagonal(columnOnMap,lineOnMap)
+    // checkVictory(columnOnMap,lineOnMap)
     checkDraw()
     changePlayer()
 
     
 }
 
-const columns = document.querySelectorAll('.column')
+const columns = document.querySelectorAll('.column') 
 
 columns.forEach(item => {
     item.addEventListener('click', moveCircle)
